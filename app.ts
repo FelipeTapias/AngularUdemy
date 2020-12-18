66d14d193f9d0425a4137ab41702f722f057b67d
Andrés Felipe Tapias Tuberquia <atapias@sistecredito.com>
2020-12-18 11:58:55 -0500

    Se termina la introduccion a TypeScript

--- app.ts
+++ app.ts
@@ -1,23 +1,5 @@
 ( () => {
  
-    const retirarDinero = (montoRetirar: number): Promise<number> => {
-        
-        let dineroActual = 1000;
-
-        return new Promise( (resolve, reject) => {
-            if(montoRetirar > dineroActual) {
-                reject('No hay suficiente dinero')
-            }else{
-                dineroActual -= montoRetirar;
-                resolve( dineroActual );
-            }
-        });
-
-
-    }
-
-    retirarDinero( 500 )
-        .then( montoActual => console.log(`Me queda ${ montoActual }`))
-        .catch( console.warn );
+    
 
 })();
